e319d91c782211e2fc394b003cd45b0d1c65cf86
George P <killerkip07@mail.ru>
2022-08-11 09:34:41 +0300

    4

---
+++ 1/main.java
@@ -0,0 +1,63 @@
+public class main {
+
+    public static void main(String[] args) {
+        
+
+        // 1. Выбросить случайное целое число и сохранить в i
+
+        double x = (Math.random()*100000);
+        int i = (int) x;
+        System.out.println(i);
+
+        // 2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+
+        int n = Integer.toBinaryString(i).length();
+        System.out.println(n);
+
+        // 3. Найти все кратные n числа большие i и сохранить в массив m1
+    
+        // int indexMax = Integer.MAX_VALUE;
+        // System.out.println(Integer.MAX_VALUE);
+        // int arraySize = 0;
+
+        // for (int j = indexMax; j > i; j--) {
+        //     if (j%n == 0) {
+        //         arraySize++;
+        //     }
+        // }
+
+        // int[] m1 = new int[arraySize];
+        // int arrayindexMax = arraySize;
+        // for (int j = i; j <= indexMax; j++) {
+        //     if (j%n == 0) {
+        //         m1[arraySize-arrayindexMax] = j ;
+        //         System.out.println(m1[arraySize-arrayindexMax]);
+        //         arrayindexMax--;
+        //     }
+        // }   
+
+        //4. Найти все некратные n числа меньшие i и сохранить в массив m2
+
+        
+        int indexMin = Integer.MIN_VALUE;
+        System.out.println(Integer.MIN_VALUE);
+        int arraym2Size = 0;
+
+        for (int j = indexMin; j < i; j++) {
+            if (j%n != 0) {
+                arraym2Size++;
+            }
+        }
+        System.out.println(arraym2Size);
+        int[] m2 = new int[arraym2Size];
+        int arrayindexMin = arraym2Size;
+        for (int j = indexMin; j < i; j++) {
+            if (j%n != 0) {
+                m2[arraym2Size-arrayindexMin] = j ;
+                System.out.println(m2[arraym2Size-arrayindexMin]);
+                arrayindexMin--;
+            }
+        }   
+    }
+
+}
